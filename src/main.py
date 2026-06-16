f64c7aba6eb51ab3940543fd96d66530ba866146
oz <zaleambo@gmail.com>
2026-06-16 21:46:51 +0300

    Add initial HTML and CSS files for Tolkien Fan Club site
    - Created index.html with structured content including headings, lists, and a blockquote.
    - Added index.css for styling the webpage with a dark theme, custom fonts, and scrollbar styles.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import os, shutil
+import os, shutil, sys
 from utils.helper_functions import extract_title
 from utils.blocknode import *
 
@@ -6,21 +6,26 @@ ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 def main():
-    
+
+    args = sys.argv
+    basepath = args[1] if len(args) >= 2 else "/"
+
     static_src_path, static_des_path = os.path.join(
         ROOT_DIR, "src/static"
-    ), os.path.join(ROOT_DIR, "public")
+    ), os.path.join(ROOT_DIR, "docs")
     clean_and_copy_all_contents(
         static_src_path, static_des_path
     )  # copy the static files
 
     md_src_path, md_des_path, template_path = (
         os.path.join(ROOT_DIR, "content"),
-        os.path.join(ROOT_DIR, "public"),
+        os.path.join(ROOT_DIR, "docs"),
         os.path.join(ROOT_DIR, "src/template.html"),
     )
+    
+   
 
-    generate_pages_recursive(md_src_path, template_path, md_des_path)
+    generate_pages_recursive(md_src_path, template_path, md_des_path , basepath)
 
 
 def clean_destination_directory(path):
@@ -62,7 +67,7 @@ def clean_and_copy_all_contents(src, destination):
     copy_all_contents(src, destination)
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}.")
 
     with open(from_path, "r") as f:
@@ -71,7 +76,12 @@ def generate_page(from_path, template_path, dest_path):
         template = f.read()
 
     title, content = extract_title(markdown), markdown_to_html_node(markdown).to_html()
-    html_file = template.replace("{{ Title }}", title).replace("{{ Content }}", content)
+    html_file = (
+        template.replace("{{ Title }}", title)
+        .replace("{{ Content }}", content)
+        .replace('href="/', f'href="{basepath}')
+        .replace('src="/', f'src="{basepath}')
+    )
 
     directory_path, _ = os.path.split(dest_path)
     os.makedirs(directory_path, exist_ok=True)
@@ -80,7 +90,8 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, "w") as f:
         f.write(html_file)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path ,basepath):
 
     for item in os.listdir(dir_path_content):
 
@@ -88,11 +99,12 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         des_path = os.path.join(dest_dir_path, item)
 
         if os.path.isfile(item_path):
-            des_path=des_path.replace(".md", ".html")
-            generate_page(from_path=item_path , template_path=template_path ,dest_path=des_path )
+            des_path = des_path.replace(".md", ".html")
+            generate_page(
+                from_path=item_path, template_path=template_path, dest_path=des_path, basepath=basepath
+            )
         else:
-            generate_pages_recursive(item_path , template_path , des_path)
-
+            generate_pages_recursive(item_path, template_path, des_path , basepath)
 
 
 if __name__ == "__main__":
